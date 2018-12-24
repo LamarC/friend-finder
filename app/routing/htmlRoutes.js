3cacb8495c939af4ec2025bddaa2927185158fac
LamarC <dviann89@gmail.com>
2018-12-24 00:26:19 -0600

    Routes fixed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 module.exports = function(app){
     app.get("/survey", function(req, res){
-        res.sendFile(path.join(__dirname + "../public/survey.html"));
+        res.sendFile(path.join(__dirname + "./public/survey.html"));
     });
 
     app.use(function (req, res) {
-        res.sendFile(path.join(__dirname + "../public/index.html"));
+        res.sendFile(path.join(__dirname + "./public/index.html"));
       });
 };
